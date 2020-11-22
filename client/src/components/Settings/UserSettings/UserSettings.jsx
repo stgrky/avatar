8d0aa07c3907b6cc3bb0d2480fdb316d49f1f428
Grant Kyle <sgrantkyle@gmail.com>
2020-11-22 16:27:29 -0600

    onchange added to profile name

--- client/src/components/Settings/UserSettings/UserSettings.jsx
+++ client/src/components/Settings/UserSettings/UserSettings.jsx
@@ -3,30 +3,42 @@ import UserAvatarUploadPicker from "../../UserAvatarUploadPicker";
 import axios from "axios";
 import "./style.scss";
 
-const UserSettings = ({}) => {
+const UserSettings = () => {
+  const [contact, setContact] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    type: "personal",
+  });
+
+  const { name, email, phone, type } = contact;
+
+  const onChange = (e) =>
+    setContact({ ...contact, [e.target.name]: e.target.value });
+
+    console.log("name")
+  console.log(name);
   return (
     <div>
-      <div className={"tg-user-settings__tab-title"}>
-        Basic Info
-      </div>
+      <div className={"tg-user-settings__tab-title"}>Basic Info</div>
       <div className={"d-flex flex-row"}>
         <div className={"tg-user-settings__left"}>
           <div className={"tg-user-settings__avatar-box"}>
             <UserAvatarUploadPicker
-              // imageUrl={portfolio.portfolio_logo_image_url}
-              // onChange={(err, data) => {
-              //   if (err) {
-              //     console.log("error!!!");
-              //   }
-              //   portfolio.portfolio_logo_image_url = data;
-              //   setPortfolio({ ...portfolio });
-              // }}
-              // missingImageBackupInitialsString={portfolio.display_name}
-              // cloudinaryUploadFolder={`/portfolios/${portfolio.portfolio_id}`}
-              // handleAvatarDelete={() => {
-              //   portfolio.portfolio_logo_image_url = "";
-              //   setPortfolio({ ...portfolio });
-              // }}
+            // imageUrl={portfolio.portfolio_logo_image_url}
+            // onChange={(err, data) => {
+            //   if (err) {
+            //     console.log("error!!!");
+            //   }
+            //   portfolio.portfolio_logo_image_url = data;
+            //   setPortfolio({ ...portfolio });
+            // }}
+            // missingImageBackupInitialsString={portfolio.display_name}
+            // cloudinaryUploadFolder={`/portfolios/${portfolio.portfolio_id}`}
+            // handleAvatarDelete={() => {
+            //   portfolio.portfolio_logo_image_url = "";
+            //   setPortfolio({ ...portfolio });
+            // }}
             />
           </div>
         </div>
@@ -35,37 +47,27 @@ const UserSettings = ({}) => {
           <div className={"d-flex flex-column"}>
             <div className={"d-flex flex-row"}>
               <div
-                className={
-                  "form-group w-100 mr-3 tg-user-settings__container"
-                }
+                className={"form-group w-100 mr-3 tg-user-settings__container"}
               >
-                <input className="tg-user-settings__input"
-                  // placeholder="Enter a name"
-                  // type="text"
-                  // name="portfolio_name"
+                <input
+                  className="tg-user-settings__input"
+                  placeholder="Enter a name"
+                  type="text"
+                  name="name"
                   // errorMessage={"Please enter a name"}
-                  // handleInputChange={(e) => {
-                  //   setPortfolio({
-                  //     ...portfolio,
-                  //     display_name: e.target.value,
-                  //   });
-                  // }}
-                  // displayName={"Portfolio name"}
+                  onChange={onChange}
+                  displayName={"Portfolio name"}
                   // inputClassNames={"input-text--primary"}
-                  // value={portfolio.display_name}
+                  value={name}
                 />
               </div>
             </div>
             <div className={"d-flex flex-row"}>
               <div
-                className={
-                  "d-flex flex-column tg-user-settings__title-input"
-                }
+                className={"d-flex flex-column tg-user-settings__title-input"}
               ></div>
               <div
-                className={
-                  "form-group w-100 mr-3 tg-user-settings__container"
-                }
+                className={"form-group w-100 mr-3 tg-user-settings__container"}
               ></div>
             </div>
           </div>
---
+++ routes/indexApiV1.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
+const Profile = require("../models/Profile");
+
